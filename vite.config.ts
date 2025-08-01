f8fe70cfd4e71012f06e7b221cee11d427d51f36
yosefabebe <yosefabebe2008@gmail.com>
2025-08-01 11:54:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ✅ IMPORTANT for Netlify or GitHub Pages
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['@emailjs/browser'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@emailjs/browser'], // ✅ avoid build crash
+    },
   },
 })
