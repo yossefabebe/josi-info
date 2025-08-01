d49a9722d52383fda64717bb91da4c01ad999206
yosefabebe <yosefabebe2008@gmail.com>
2025-08-01 15:44:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ Correct for Netlify
+  base: '/', // ✅ For Netlify (or custom domain)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
