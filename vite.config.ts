0a09510c7a1fb23d8b2c90d3f90b185ee6e24de0
yosefabebe <yosefabebe2008@gmail.com>
2025-08-01 14:34:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+ base: '/yosef.info/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
