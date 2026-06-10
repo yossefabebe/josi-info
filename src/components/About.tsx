fc66983b0d607922f0bae569ed46332b45ddcdc6
yosefabebe <yosefabebe2008@gmail.com>
2026-06-10 00:08:16 -0700

    Change text color for educational qualifications

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -126,7 +126,7 @@ const About: React.FC = () => {
              <div className="flex items-start space-x-4">
               <div className="flex-shrink-0 w-3 h-3 bg-accent-600 rounded-full mt-2"></div>
               <div>
-                <div className="font-semibold text-purple-900">Educational Qualifications</div>
+                <div className="font-semibold text-purple-1000">Educational Qualifications</div>
                 <div className="text-green-600 font-medium">Master's in Project Management</div>
                 <div className="text-sm text-gray-600">Queens' University College - In Progress</div>
               </div>
